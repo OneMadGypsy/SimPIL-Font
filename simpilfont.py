4a8d4e21fd4fa563ff478bf9506cea2839983eaf
Michael Guidry <onemadgypsy@gmail.com>
2023-12-22 20:43:27 -0600

    Update simpilfont.py

--- simpilfont.py
+++ simpilfont.py
@@ -4,64 +4,33 @@ from   PIL     import ImageFont
 import os
 
 class SimPILFont:
-    #https://pillow.readthedocs.io/en/stable/reference/ImageFont.html#PIL.ImageFont.truetype
+    # https://pillow.readthedocs.io/en/stable/reference/ImageFont.html#PIL.ImageFont.truetype
     ENCODINGS = "unic", "symb", "DOB", "ADBE", "ADBC", "armn", "sjis", "gb", "big5", "ans", "joha", "lat1"
-
-    ## STATIC
-
-    @staticmethod
-    def bestface(face:str, options:tuple) -> str:
-        face = face.replace(' ', '')
         
-        for dflt in ('regular', 'book'):
-            if dflt in options: break
-        else: dflt = options[0]
-        
-        if options: 
-            if not face in options:
-                face = dflt
-        else: face = ''
-                
-        return face
-        
-    @staticmethod      
-    def metadata(font:str) -> tuple:
-        fmly, face, size = [], [], 0
-        
-        for part in font.split(' '):
-            if part.isdigit(): size = int(part)
-            else             : (face, fmly)[part != part.lower()].append(part)
-                
-        return ' '.join(fmly), ' '.join(face), size
-        
-    ## PROPERTIES    
-        
-    @property #current family
-    def family(self) -> str:
+    @property
+    def family(self) -> str: 
         return self._family
         
-    @property #current face
-    def face(self) -> str:
+    @property
+    def face(self) -> str: 
         return self._face
         
-    @property #current size
-    def size(self) -> int:
+    @property
+    def size(self) -> int: 
         return self._size
         
-    @property #current path
-    def path(self) -> str:
+    @property
+    def path(self) -> str: 
         return self._path
         
-    @property #list of supported faces for the current font 
-    def faces(self) -> tuple:
-        return tuple(self._faces)
-           
-    @property #current ImageFont instance
-    def font(self) -> ImageFont.FreeTypeFont:
+    @property
+    def font(self) -> ImageFont.FreeTypeFont: 
         return self._font
         
-    ## DUNDER
-        
+    @property # supported faces for the current font 
+    def facetypes(self) -> tuple: 
+        return tuple(self._facetypes)
+           
     def __init__(self, fontdirs:Iterable):
         self._fontdirs = fontdirs if isinstance(fontdirs, list|tuple) else (fontdirs, )
         
@@ -71,25 +40,32 @@ class SimPILFont:
     def __call__(self, font:str, encoding:str='unic'):
         encoding = encoding if encoding in SimPILFont.ENCODINGS else 'unic'
         
-        #get details
-        family, face, size = SimPILFont.metadata(font)
+        # get details
+        family, face, size = [], [], 0
         
-        #use detail else nochange else default
-        self._family  = family or getattr(self, '_family', 'Arial')
-        self._size    = size   or getattr(self, '_size'  , 12)
-        self._face    = face   or getattr(self, '_face'  , 'regular')
+        for part in font.split(' '):
+            if part.isdigit(): size = int(part)
+            else             : (face, family)[part != part.lower()].append(part)
+                
+        family, face = ' '.join(family), ' '.join(face)
         
-        faces       = dict()
-        found       = False
-        self._faces = []
+        # use detail else nochange else default
+        self._family    = family or getattr(self, '_family', 'Arial')
+        self._size      = size   or getattr(self, '_size'  , 12)
+        self._face      = face   or getattr(self, '_face'  , 'regular')
+        self._facetypes = []
         
+        faces = dict()
+        found = False
+        
+        # find font via family and face
         for directory in self._fontdirs:
             for fn in iglob(fr'{directory}**/*.ttf', recursive=True):
                 fn = os.path.abspath(fn)
-                try:
-                    ttf = ImageFont.truetype(font=fn)
+                
+                try   : ttf = ImageFont.truetype(font=fn)
                 except: ...
-                else:
+                else  :
                     family, face = ttf.getname() 
                     
                     if (family != self._family) and found:
@@ -98,39 +74,43 @@ class SimPILFont:
                     if family == self._family:
                         found = True
                         face  = face.lower()
-                        self._faces.append(face)
+                        self._facetypes.append(face)
                         faces[face.replace(' ', '')] = fn
                     
-            if found: 
-                break
+            if found: break
         else: 
             raise ValueError('No font for you!')
             
-        #juggle face type
-        face = SimPILFont.bestface(self._face, tuple(faces))
+        # get best face
+        options = tuple(faces)
+        face    = self._face.replace(' ', '')
         
-        for option in self._faces:
-            if option.replace(' ', '') == face:
-                self._face = option
-                break
+        if not face in options:
+            for face in ('regular', 'book'):
+                if face in options: 
+                    self._face = face
+                    break
+            else: 
+                face       = options[0]
+                self._face = self._facetypes[0]
         
+        # get and use path
         self._path  = faces.get(face, '')        
         self._font  = ImageFont.truetype(self._path, self._size, encoding=encoding)
         
+        # inline
         return self
         
-    ## PROXIES
-       
-    #basic bbox
+    # basic bbox
     def bbox(self, text:str) -> tuple:
         return self._font.getbbox(text)
     
-    #smallest possible bbox
+    # smallest possible bbox
     def min_bbox(self, text:str) -> tuple:
         x, y, w, h = self._font.getbbox(text)
         return -x, -y, w-x, h-y
         
-    #(right/bottom) margins mirror the (left/top) margins, respectively
+    # (right/bottom) margins mirror the (left/top) margins, respectively
     def max_bbox(self, text:str) -> tuple:
         x, y, w, h = self._font.getbbox(text)
         return 0, 0, w+x, h+y
